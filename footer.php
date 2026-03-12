1cb4839eb35808f315ecd04f9cf7daf6c943a6ec
shradhasapat <shradhasapat191991@gmail.com>
2026-03-12 16:36:30 +0530

    footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         <!-- ABOUT -->
         <div class="space-y-4">
             <div class="bg-gray-100 rounded-lg ">
-                    <h1 class="text-3xl font-bold text-center text-gradient">ALORNIX</h1>
+                    <h1 class="text-3xl font-bold text-center text-gradient">Finworld Solution</h1>
                 </div>
             <p class="text-sm text-gray-400 leading-relaxed">
                 Money Trust Financial Services provides trusted financial guidance, 
@@ -41,26 +41,18 @@
 
         <!-- NEWSLETTER + CONTACT -->
         <div class="space-y-4">
-            <h3 class="text-white text-lg font-semibold mb-5">Stay Updated</h3>
-            <p class="text-sm text-gray-400 mb-4">
-                Subscribe to get financial updates and loan offers.
-            </p>
-            <form class="flex">
-                <input type="email" placeholder="Your email"
-                    class="bg-white/10 backdrop-blur-md border border-gray-600 px-4 py-2 rounded-l-lg w-full focus:outline-none">
-                <button class="bg-blue-600 px-4 rounded-r-lg hover:bg-blue-700 transition">Subscribe</button>
-            </form>
+            
 
             <div class="mt-6 space-y-2 text-sm text-gray-400">
-                <p>📞 +91 8080435343</p>
-                <p>📧 info@moneytrust.in</p>
+                <p>📞+91 7588086103</p>
+                <p>📧 info@finworldsolution.org</p>
             </div>
         </div>
 
     </div>
 
-    <!-- GLASS DISCLAIMER SECTION -->
-    <div class="max-w-7xl mx-auto px-6 mt-12">
+    
+    <!-- <div class="max-w-7xl mx-auto px-6 mt-12">
         <div class="bg-white/5 backdrop-blur-md border border-white/10 rounded-xl p-6 text-xs text-gray-400 space-y-3">
             <p>
                 Registration No.: AMFI Registration No. ARN 191686 | AMFI SIF Registration No. ARN 191686 | APMI APRN Registration No. APRN07450
@@ -72,11 +64,11 @@
                 Investments in securities markets are subject to market risks. Terms and conditions of the website are applicable.
             </p>
         </div>
-    </div>
+    </div> -->
 
     <!-- COPYRIGHT -->
     <div class="text-center text-gray-500 text-sm mt-10 pb-8">
-        Copyright © 2026 Money Trust Financial Services. All Rights Reserved.
+        Copyright © 2026 Finworld Solution. All Rights Reserved.
     </div>
 
 </footer>
