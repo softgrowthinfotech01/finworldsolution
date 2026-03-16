35c996486da207ef89c3f58d95e350355afe74d9
shradhasapat <shradhasapat191991@gmail.com>
2026-03-16 15:41:59 +0530

    GIT

--- header.php
+++ header.php
@@ -234,7 +234,7 @@ shadow lg:shadow-none">
 
                 </div>
 
-                <a href="gallery" class="block space-grotesk hover:text-blue-700">Gallery</a>
+                <a href="gallery" class="block space-grotesk hover:text-blue-700"> GALLERY</a>
 
 
 
