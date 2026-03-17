6e36d634cd7bcee2c7dbadb15ea8972017abce68
shradhasapat <shradhasapat191991@gmail.com>
2026-03-17 12:37:52 +0530

    change

--- career.php
+++ career.php
@@ -81,7 +81,7 @@ Attractive incentives and recognition for outstanding performance.
 </div>
 
 </div>
-<div class="m-10">
+<div class="m-10 text-center">
    
   <a href="career-form"
 class="inline-block mt-6 bg-gray text-blue-900 px-6 py-3 rounded-lg font-semibold hover:bg-blue-100 transition">
